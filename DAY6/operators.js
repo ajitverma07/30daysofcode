02152c65cb76d171ffa67a3637804c182a1b2b78
ajitverma07 <143831302+ajitverma07@users.noreply.github.com>
2023-09-13 18:59:26 +0530

    Add files via upload

---
+++ DAY6/operators.js
@@ -0,0 +1,16 @@
+//operators in java scripts
+let a = 20;
+let b = 10;
+//arithmatic operators
+console.log(a+b) //for adding two numbers
+console.log(a-b) // for substract two numbers
+console.log(a*b) // for multiply two numbers
+console.log(a\b) // for dividing two number
+console.log(a%b) // for get rmainder
+console.log(a++) // it icreaments "a" by 1
+console.log(a--)  // it decreaments "a" by 1
+
+//logical operators
+console.log(a==b && a != b) // (and operator) output=false | both are must be true
+console.log(a==b || a != b) // (or operator) output=true | one of both be true
+console.log(a==b ! a!=b) // (not operator) output=flase | if output is true it ll give false if output is false it ll give true
