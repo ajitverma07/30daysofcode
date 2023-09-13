ba98acb326eea044702bd2f9f430536e94213d41
ajitverma07 <143831302+ajitverma07@users.noreply.github.com>
2023-09-13 22:24:27 +0530

    Add files via upload

---
+++ DAY7/conditionalexpessions.js
@@ -0,0 +1,16 @@
+let a = prompt("enter your age: ");
+a = Number.parseInt(a) //parseint converts the string to number
+// let b = length();
+// console.log(b)
+
+//if-else statments
+if(a>20){
+    alert("you are qualified")
+}else{
+    alert("try again")
+
+
+}
+
+
+
---
+++ DAY7/ternaryoperator.js
@@ -0,0 +1,2 @@
+a = prompt("enter your age :");
+console.log("you are" ,(a<20? "not qualified" : "qualified"))
